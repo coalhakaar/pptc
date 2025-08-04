a9d386d9036d6c34652f95906c2ba9ab9f0110f3
hakaar <placidityisepic@gmail.com>
2025-08-04 19:14:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[~] Downloading pptc"
-wget -O https://github.com/coalhakaar/pptc/releases/download/v1.0.0/pptc
+wget -O https://raw.githubusercontent.com/coalhakaar/pptc/refs/heads/main/pptc
 chmod +x pptc
 echo "[>] Moving file" 
 sudo mv pptc /usr/local/bin/
